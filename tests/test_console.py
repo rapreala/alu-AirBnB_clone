94ebaee7982aa155324266e21a3b8e7152181937
rapreala <r.apreala@alustudent.com>
2023-05-23 17:22:38 +0000

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -82,44 +82,50 @@ class TestHBNBCommand(unittest.TestCase):
             self.assertFalse(self.console.onecmd("update BaseModel"))
             self.assertEqual(f.getvalue(), "** instance id missing **\n")
 
-    def test_help(self):
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.assertTrue(self.console.onecmd("help"))
-            self.assertTrue(len(f.getvalue()) > 0)
+    def test_all_method_present(self):
+        self.assertTrue(hasattr(self.console, "do_BaseModel.all"))
+        self.assertTrue(hasattr(self.console, "do_Review.all"))
+        self.assertTrue(hasattr(self.console, "do_User.all"))
+        self.assertTrue(hasattr(self.console, "do_State.all"))
+        self.assertTrue(hasattr(self.console, "do_City.all"))
+        self.assertTrue(hasattr(self.console, "do_Amenity.all"))
+        self.assertTrue(hasattr(self.console, "do_Place.all"))
 
-            self.assertTrue(self.console.onecmd("help quit"))
-            self.assertTrue(len(f.getvalue()) > 0)
+    def test_count_method_present(self):
+        self.assertTrue(hasattr(self.console, "do_BaseModel.count"))
+        self.assertTrue(hasattr(self.console, "do_User.count"))
+        self.assertTrue(hasattr(self.console, "do_State.count"))
+        self.assertTrue(hasattr(self.console, "do_City.count"))
+        self.assertTrue(hasattr(self.console, "do_Place.count"))
+        self.assertTrue(hasattr(self.console, "do_Amenity.count"))
+        self.assertTrue(hasattr(self.console, "do_Review.count"))
 
-            self.assertTrue(self.console.onecmd("help EOF"))
-            self.assertTrue(len(f.getvalue()) > 0)
+    def test_show_method_present(self):
+        self.assertTrue(hasattr(self.console, "do_BaseModel.show"))
+        self.assertTrue(hasattr(self.console, "do_User.show"))
+        self.assertTrue(hasattr(self.console, "do_State.show"))
+        self.assertTrue(hasattr(self.console, "do_City.show"))
+        self.assertTrue(hasattr(self.console, "do_Amenity.show"))
+        self.assertTrue(hasattr(self.console, "do_Place.show"))
+        self.assertTrue(hasattr(self.console, "do_Review.show"))
 
-            self.assertTrue(self.console.onecmd("help create"))
-            self.assertTrue(len(f.getvalue()) > 0)
+    def test_destroy_method_present(self):
+        self.assertTrue(hasattr(self.console, "do_BaseModel.destroy"))
+        self.assertTrue(hasattr(self.console, "do_User.destroy"))
+        self.assertTrue(hasattr(self.console, "do_City.destroy"))
+        self.assertTrue(hasattr(self.console, "do_State.destroy"))
+        self.assertTrue(hasattr(self.console, "do_Place.destroy"))
+        self.assertTrue(hasattr(self.console, "do_Amenity.destroy"))
+        self.assertTrue(hasattr(self.console, "do_Review.destroy"))
 
-            self.assertTrue(self.console.onecmd("help show"))
-            self.assertTrue(len(f.getvalue()) > 0)
-
-            self.assertTrue(self.console.onecmd("help destroy"))
-            self.assertTrue(len(f.getvalue()) > 0)
-
-            self.assertTrue(self.console.onecmd("help all"))
-            self.assertTrue(len(f.getvalue()) > 0)
-
-            self.assertTrue(self.console.onecmd("help count"))
-            self.assertTrue(len(f.getvalue()) > 0)
-
-            self.assertTrue(self.console.onecmd("help update"))
-            self.assertTrue(len(f.getvalue()) > 0)
-
-    def test_default(self):
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.assertFalse(self.console.onecmd("non_existent_command"))
-            self.assertEqual(f.getvalue(), "*** Unknown syntax: non_existent_command\n")
-
-    def test_emptyline(self):
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.assertIsNone(self.console.emptyline())
-
+    def test_update_method_present(self):
+        self.assertTrue(hasattr(self.console, "do_BaseModel.update"))
+        self.assertTrue(hasattr(self.console, "do_User.update"))
+        self.assertTrue(hasattr(self.console, "do_State.update"))
+        self.assertTrue(hasattr(self.console, "do_City.update"))
+        self.assertTrue(hasattr(self.console, "do_Place.update"))
+        self.assertTrue(hasattr(self.console, "do_Amenity.update"))
+        self.assertTrue(hasattr(self.console, "do_Review.update"))
 
 if __name__ == '__main__':
     unittest.main()
